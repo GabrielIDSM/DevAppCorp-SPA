e3db91ad0fb2d8b2de3be92faa578fae3f8e49fa
gabriel <gabrielinacio@id.uff.br>
2022-06-17 19:59:32 -0300

    Updates

---
+++ precache-manifest.22336e8e712bc74d824175c7e3098107.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "24d3b5dbb5807343567b",
+    "url": "/DevAppCorp-SPA/static/css/main.4c7473fd.chunk.css"
+  },
+  {
+    "revision": "24d3b5dbb5807343567b",
+    "url": "/DevAppCorp-SPA/static/js/main.24d3b5db.chunk.js"
+  },
+  {
+    "revision": "10b78bcfadf2ac7af537",
+    "url": "/DevAppCorp-SPA/static/js/1.10b78bcf.chunk.js"
+  },
+  {
+    "revision": "96ba338e08e484d4e1cb",
+    "url": "/DevAppCorp-SPA/static/js/runtime~main.96ba338e.js"
+  },
+  {
+    "revision": "52ae2f4525b71025369aeffc97f94f0d",
+    "url": "/DevAppCorp-SPA/static/media/logo.52ae2f45.svg"
+  },
+  {
+    "revision": "ddcfd22b011e4fb1ec8318427ccbf5f0",
+    "url": "/DevAppCorp-SPA/static/media/github.ddcfd22b.png"
+  },
+  {
+    "revision": "6d1bc9d0f861246e97b39da7a5c79a6d",
+    "url": "/DevAppCorp-SPA/static/media/outlook.6d1bc9d0.png"
+  },
+  {
+    "revision": "306147e6cd12b05f32e220f70677ad77",
+    "url": "/DevAppCorp-SPA/static/media/reddit.306147e6.png"
+  },
+  {
+    "revision": "86f43cd597469648cc8799f5c0f4b739",
+    "url": "/DevAppCorp-SPA/static/media/zoom.86f43cd5.png"
+  },
+  {
+    "revision": "f2c0ac018f89410c9322cee9c1a37b40",
+    "url": "/DevAppCorp-SPA/index.html"
+  }
+];
